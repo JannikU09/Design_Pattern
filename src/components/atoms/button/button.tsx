dc22cd07303ed40959b33d5ca7ab95d70db1929a
JannikU09 <j.uphoff@innfactory.de>
2026-03-09 15:30:53 +0100

    Button-Komponente hinzugefügt, um das MUI-Icon darzusetellen, die Farbe und die BackgroundColor zu ändern.

--- src/components/atoms/button/button.tsx
+++ src/components/atoms/button/button.tsx
@@ -1,7 +1,31 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import "./button.css";
 
-export const Button = () => {
+export const Button = ({
+    Icon,
+    color,
+    bgColor,
+    size,
+}: {
+    Icon: any;
+    color: string;
+    bgColor: string;
+    size: number;
+}) => {
+
     return (
-        <button className="button"></button>
+        <button
+            className="button"
+            id="button"
+            style={{
+                color: color,
+                backgroundColor: bgColor,
+                height: size + "px",
+                width: 10+size + "px",
+            }}
+        >
+            <Icon />
+        </button>
     )
 }
