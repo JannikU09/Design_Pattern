a64bc9b0d06b9e505968351626927f517ebb1d41
JannikU09 <j.uphoff@innfactory.de>
2026-03-10 13:04:28 +0100

    Tabelle implementiert, um die Elemente in der NavBar besser anzuordnen.

--- src/components/organisms/navBar/navBar.tsx
+++ src/components/organisms/navBar/navBar.tsx
@@ -10,9 +10,19 @@ export const NavBar = () => {
 
     return (
         <div className="navBar">
-            <Button Icon={ArrowBackIcon} color="white" bgColor="#115688" size={30} />
-            <Headline text="photo"/>
-            <Button Icon={ReplayIcon} color="white" bgColor="#115688" size={30} />
+            <table style={{ width: "100%" }}>
+                <tbody>
+                    <td style={{ width: "15%" }} align="left">
+                        <Button Icon={ArrowBackIcon} color="white" bgColor="#115688" size={20} />
+                    </td>
+                    <td style={{ width: "70%" }} align="center">
+                        <Headline text="photo" />
+                    </td>
+                    <td style={{ width: "15%" }} align="right">
+                        <Button Icon={ReplayIcon} color="white" bgColor="#115688" size={20} />
+                    </td>
+                </tbody>
+            </table>
         </div>
     )
 }
