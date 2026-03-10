85227418e5acf3e0c32b3498344076ad9a02e8a9
JannikU09 <j.uphoff@innfactory.de>
2026-03-10 08:57:50 +0100

    DescriptionLine-Komponente hinzugefügt, in der die Komponenten von Username und Description zusammengeführt werden.

---
+++ src/components/moleclues/descriptionLine/descriptionLine.tsx
@@ -0,0 +1,13 @@
+import "./descriptionLine.css";
+import { UserName } from "../../atoms/userName/userName";
+import { Description } from "../../atoms/description/description";
+
+export const DescriptionLine = () => {
+    return (
+        <div className="descriptionLine">
+                <UserName name="innFactory" />
+                &nbsp;
+                <Description text="Hello this is a very intersting text." />
+        </div>
+    )
+}
