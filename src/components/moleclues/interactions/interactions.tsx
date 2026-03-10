f21b3744c518af8a31d85d2a3a3881fc59936fec
JannikU09 <j.uphoff@innfactory.de>
2026-03-10 12:54:54 +0100

    Tabelle eingefügt für eine strukturierte Aufteilung der einzelnen Elemente.

--- src/components/moleclues/interactions/interactions.tsx
+++ src/components/moleclues/interactions/interactions.tsx
@@ -9,9 +9,23 @@ import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutline
 export const Interactions = () => {
     return (
         <div className="interactions">
-            <Button Icon={FavoriteBorderOutlinedIcon} color="#a5a7aa" bgColor="white" size={30} />
-            <Button Icon={ChatBubbleOutlineOutlinedIcon} color="#a5a7aa" bgColor="white" size={30} />
-            <Button Icon={MoreHorizOutlinedIcon} color="#a5a7aa" bgColor="white" size={30} />
+            <table style={{width: "100%"}}>
+                <tbody>
+                    <tr>
+                        <td style={{width: "20%"}}>
+                            <Button Icon={FavoriteBorderOutlinedIcon} color="#a5a7aa" bgColor="white" size={30} />
+                        </td>
+                        <td style={{width: "20%"}}>
+                            <Button Icon={ChatBubbleOutlineOutlinedIcon} color="#a5a7aa" bgColor="white" size={30} />
+                        </td>
+                        <td style={{width: "60%"}} align="right">
+                            <Button Icon={MoreHorizOutlinedIcon} color="#a5a7aa" bgColor="white" size={30} />
+                        </td>
+                    </tr>
+
+                </tbody>
+
+            </table>
         </div>
     )
 }
