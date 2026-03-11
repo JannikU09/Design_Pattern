613ce1ea304632ce65e6701df2a8e0856ae7c890
JannikU09 <j.uphoff@innfactory.de>
2026-03-11 13:31:45 +0100

    <span>-Tag mit der UserName-Komponente und der Description-Komponente ausgetauscht.

--- src/components/moleclues/descriptionLine/descriptionLine.tsx
+++ src/components/moleclues/descriptionLine/descriptionLine.tsx
@@ -1,13 +1,16 @@
 import "./descriptionLine.css";
 import "../../atoms/userName/userName.css";
+import { UserName } from "../../atoms/userName/userName";
+import { Description } from "../../atoms/description/description";
 
 export const DescriptionLine = () => {
     return (
         <div className="descriptionLine">
             <div>
                 <p>
-                    <span className="userName">innFactory </span>
-                    <span>Hello, this is the most interesting text you will ever read.</span>
+                    <UserName name="innFactory"/>
+                    &nbsp;
+                    <Description text="Hello, this is the most interesting text you will ever read."/>
                 </p>
             </div>
         </div>
