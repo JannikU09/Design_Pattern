dc59b39fa056f305449027175770d004df763dd9
JannikU09 <j.uphoff@innfactory.de>
2026-03-11 12:34:03 +0100

    Button und die Anzeige der Likes in jeweils ein <span>-Tag verschoben.

--- src/components/moleclues/likesline/likesline.tsx
+++ src/components/moleclues/likesline/likesline.tsx
@@ -1,5 +1,5 @@
 import "./likesline.css";
-import { Likes } from "../../atoms/likes/likes";
+import { likes } from "../../../utils/likes";
 import { Button } from "../../atoms/button/button";
 // Icon
 import FavoriteIcon from '@mui/icons-material/Favorite';
@@ -7,8 +7,12 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 export const Likesline = () => {
     return (
         <div className="likesline">
-            <Button Icon={FavoriteIcon} color="#115688" bgColor="white" size={10} />
-            <Likes /> Likes
+            <span>
+                <Button Icon={FavoriteIcon} color="#115688" bgColor="white" size={10} />
+            </span>
+            <span>
+                {likes()} Likes
+            </span>
         </div>
     )
 }
