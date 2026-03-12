fd296645edfc1e53cef2fe2f4a34bb768620f3a9
JannikU09 <j.uphoff@innfactory.de>
2026-03-12 15:40:39 +0100

    style: Unnötige Leerzeichen entfernt.

--- src/components/moleclues/userInfo/userInfo.tsx
+++ src/components/moleclues/userInfo/userInfo.tsx
@@ -7,9 +7,7 @@ export const UserInfo = () => {
     return (
         <div className="userInfo">
             <ProfileImg src="https://picsum.photos/45/?random=1" />
-            &nbsp;
             <UserName name="innFactory" />
-            &nbsp;
             <p style={{ color: "#a5a7aa" }}>2h</p>
         </div>
     )
