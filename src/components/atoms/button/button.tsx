e2c2bef454f3b3a74f874885a921e657bc58b604
JannikU09 <j.uphoff@innfactory.de>
2026-03-12 13:52:28 +0100

    Typ von variant, zu "primary" | "secondary" | "tertiary", geändert, um Sicherzustellen, dass variant nur die genannten annehmen kann.

--- src/components/atoms/button/button.tsx
+++ src/components/atoms/button/button.tsx
@@ -11,7 +11,7 @@ export const Button = ({
 }: {
     Icon: any;
     size: number;
-    variant: string;
+    variant: "primary" | "secondary" | "tertiary";
     color?: string;
     bgColor?: string;
 }) => {
